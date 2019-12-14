9e9853cd97f0e69fd47769243a3cc396fa08b033
Isaac W Hanson <isaac@starlig.ht>
2019-12-13 21:27:19 -0500

    soundspeed(𝜗::Number)
    calculate the speed of sound (m/s) in air of given temperature (°C)

--- src/Flutes.jl
+++ src/Flutes.jl
@@ -3,6 +3,18 @@
 Parametric Flute Modeler
 """
 module Flutes
-    # speed of sound (mm/s)
-    const 𝑐 = 346000
-end # module
+
+    """
+        soundspeed(𝜗::Number)
+
+    calculate the speed of sound (m/s)
+    in air of the given temperature 𝜗 (°C)
+    """
+    function soundspeed(𝜗::Number)
+        𝛾 = 1.400            # heat capacity ratio of air
+        𝑅 = 8.31446261815324 # molar gas constant (J/mol/K)
+        𝑀 = 0.0289647        # mean molar mass of air (kg/mol)
+        𝑐 = √(𝛾 * 𝑅/𝑀 * 273.15) * √(1 + 𝜗/273.15)
+        round(𝑐; sigdigits=4)
+    end
+end
