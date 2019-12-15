457343632c2bd0883d0f468357952e872c79613a
Isaac W Hanson <isaac@starlig.ht>
2019-12-14 21:48:45 -0500

    no rounding in functions for now

--- src/Flutes.jl
+++ src/Flutes.jl
@@ -13,8 +13,7 @@ module Flutes
         𝛾 = 1.400            # heat capacity ratio of air
         𝑅 = 8.31446261815324 # molar gas constant (J/mol/K)
         𝑀 = 0.0289647        # mean molar mass of air (kg/mol)
-        𝑐 = √(𝛾 * 𝑅/𝑀 * 273.15) * √(1 + 𝜗/273.15)
-        round(𝑐; sigdigits=4)
+        √(𝛾 * 𝑅/𝑀 * 273.15) * √(1 + 𝜗/273.15)
     end
 
     """
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,5 @@ using Flutes
 using Test
 
 @test round(Flutes.soundspeed(0.0); sigdigits=4) == 331.3
-@test round(Flutes.wavelength(440.0, 25.0); sigdigits=4) == 0.7866
+
+@test round(Flutes.wavelength(261.625565, 25.0); sigdigits=4) == 1.323
