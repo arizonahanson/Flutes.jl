a1acbb9ec00ec3e50290ef801b893720fa1524e3
Isaac W Hanson <isaac@starlig.ht>
2019-12-21 15:19:53 -0500

    Standardize units (mm,s,Hz,°C)

--- src/Flutes.jl
+++ src/Flutes.jl
@@ -1,28 +1,33 @@
 
 """
 Parametric Flute Modeler
+
+distances in millimeters
+frequencies in Hertz
+temperatures in Celsius (default 25.0°C)
+time in seconds
 """
 module Flutes
     export soundspeed, wavelength
 
     """
-        soundspeed(𝜗::Number=25.0)
+        𝑐 = soundspeed(𝜗::Number=25.0)
 
-    Calculate the speed of sound (m/s) in air of temperature 𝜗 (°C)
+    Calculate the speed of sound in air of temperature 𝜗
     """
     function soundspeed(𝜗::Number=25.0)
         𝛾 = 1.400            # heat capacity ratio of air
         𝑅 = 8.31446261815324 # molar gas constant (J/mol/K)
         𝑀 = 0.028965369      # mean molar mass of air (kg/mol)
-        √(𝛾 * 𝑅/𝑀 * 273.15) * √(1 + 𝜗/273.15)
+        √(𝛾 * 𝑅/𝑀 * 273.15) * √(1.0 + 𝜗/273.15) * 1000.0
     end
 
     """
-        wavelength(𝐹::Number=440.0, 𝜗::Number=25.0)
+        𝜆₁ = wavelength(𝐹::Number=261.6255653, 𝜗::Number=25.0)
 
-    Calculate the wavelength (m) of frequency 𝐹 (Hz) in air of temperature 𝜗 (°C)
+    Calculate the wavelength of frequency 𝐹 in air of temperature 𝜗
     """
-    function wavelength(𝐹::Number=440.0, 𝜗::Number=25.0)
+    function wavelength(𝐹::Number=261.6255653, 𝜗::Number=25.0)
         𝑐 = soundspeed(𝜗)
         𝑐/𝐹
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Flutes
 using Test
 
-@test round(Flutes.soundspeed(0.0); sigdigits=4) == 331.3
+@test round(Flutes.soundspeed(0.0); sigdigits=4) == 331300
 
-@test round(Flutes.wavelength(261.625565, 25.0); sigdigits=4) == 1.323
+@test round(Flutes.wavelength(261.6255653, 25.0); sigdigits=4) == 1323
