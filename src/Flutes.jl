f684aa8ed49f9f4cbe54d1e02c98f2f0a7e84ec2
Isaac W Hanson <isaac@starlig.ht>
2019-12-29 21:50:46 -0500

    embouchurecorrection: fit formula

--- src/Flutes.jl
+++ src/Flutes.jl
@@ -8,20 +8,23 @@ temperatures in Celsius (default 25.0°C)
 time in seconds
 """
 module Flutes
-    export Flute, soundspeed, wavelength, tubelength
+    export Flute, soundspeed, tubelength
 
     struct Flute
-        𝐹::Number
-        𝜗::Number
-        𝑑ₜ::Number
+        𝐹  # Fundamental frequency
+        𝜗  # Air temperature
+        𝑑ₜ # End tube bore diameter
+        𝑑ₑ # Embouchure diameter
+        𝑑₀ # Bore diameter at embouchure
+        ℓₑ # Embouchure height
     end
 
     """
-        𝑐 = soundspeed(𝜗::Number=25.0)
+        𝑐 = soundspeed(𝜗=25.0)
 
     Calculate the speed of sound in air of temperature 𝜗
     """
-    function soundspeed(𝜗::Number=25.0)
+    function soundspeed(𝜗=25.0)
         𝛾 = 1.400            # heat capacity ratio of air
         𝑅 = 8.31446261815324 # molar gas constant (J/mol/K)
         𝑀 = 0.028965369      # mean molar mass of air (kg/mol)
@@ -30,43 +33,43 @@ module Flutes
     end
 
     """
-        temperature_at(𝑥::Number=0.0)
+        temperature_at(𝑥=0.0)
 
     Temperature at 𝑥 distance from embouchure: Coltman (1968)
     """
-    function temperature_at(𝑥::Number=0.0)
+    function temperature_at(𝑥=0.0)
         𝑇 = 30.3 - .0077𝑥
         round(𝑇; sigdigits=3)
     end
 
     """
-        embouchurecorrection()
+        embouchurecorrection(𝑑₀=17.4, 𝑑ₑ=11.2, ℓₑ=4.3)
 
-    Correction of tube-length at embouchure
+    Correction of tube-length at embouchure of bore diameter 𝑑₀, diameter 𝑑ₑ and height ℓₑ
     """
-    function embouchurecorrection()
-        𝛥ℓₑ = 52.0
+    function embouchurecorrection(𝑑₀=17.4, 𝑑ₑ=11.2, ℓₑ=4.3)
+        𝛥ℓₑ = (ℓₑ + 𝑑ₑ) * (𝑑₀/𝑑ₑ)^2 - 0.45𝑑₀ + 10.0
     end
 
     """
-        endcorrection(𝑑ₜ::Number=19.0)
+        endcorrection(𝑑ₜ=19.0)
 
     Correction of tube-length at open-end
     """
-    function endcorrection(𝑑ₜ::Number=19.0)
+    function endcorrection(𝑑ₜ=19.0)
         𝛥ℓₜ = 0.3𝑑ₜ
     end
 
     """
-        tubelength(𝐹::Number=261.6155653, 𝜗::Number=25.0, 𝑑ₜ::Number=19.0)
+        function tubelength(𝐹=261.6155653, 𝜗=25.0, 𝑑ₜ=19.0, 𝑑₀=17.4, 𝑑ₑ=11.2, ℓₑ=4.3)
 
     Calculate tube length from embouchure-hole to open-end for fundamental frequency 𝐹,
-    with air temperature 𝜗 and open-end diameter 𝑑ₜ
+    with air temperature 𝜗, open-end diameter 𝑑ₜ, and embouchure bore diameter 𝑑₀, diameter 𝑑ₑ, height ℓₑ
     """
-    function tubelength(𝐹::Number=261.6155653, 𝜗::Number=25.0, 𝑑ₜ::Number=19.0)
+    function tubelength(𝐹=261.6155653, 𝜗=25.0, 𝑑ₜ=19.0, 𝑑₀=17.4, 𝑑ₑ=11.2, ℓₑ=4.3)
         𝑐 = soundspeed(𝜗)
         𝐿ₛ = 𝑐/2𝐹
-        𝛥ℓₑ = embouchurecorrection()
+        𝛥ℓₑ = embouchurecorrection(𝑑₀, 𝑑ₑ, ℓₑ)
         𝛥ℓₜ = endcorrection(𝑑ₜ)
         ℓₜ = 𝐿ₛ - 𝛥ℓₑ - 𝛥ℓₜ
         round(ℓₜ; digits=2)
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,3 @@ using Test
 
 @test Flutes.soundspeed(0.0) == 331300
 @test Flutes.soundspeed(25.0) == 346100
-@test round(Flutes.wavelength(261.6255653, 25.0); sigdigits=4) == 1323
