f0b3465197350ce35ebf09e53868aa2a3665e6e7
Isaac W Hanson <isaac@starlig.ht>
2020-02-01 23:30:13 -0500

    make.jl

--- src/make.jl
+++ src/make.jl
@@ -5,11 +5,13 @@ Pkg.add("JSON")
 using Mustache
 using JSON
 
-function apply(inpath, outpath, vars)
-  str = render_from_file(inpath, vars)
-  io = open(outpath, "w")
+# args = [outfile, infile, json]
+function apply(args)
+  io = open(args[1], "w")
+  str = render_from_file(args[2], JSON.parsefile(args[3]))
   write(io, str)
   close(io)
 end
 
-apply(ARGS[2], ARGS[1], JSON.parsefile(ARGS[3]))
+apply(ARGS)
+
