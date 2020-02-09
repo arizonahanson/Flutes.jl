4d26dc1b596ce0a217a84898eaa96aabdd546587
Isaac W Hanson <isaac@starlig.ht>
2020-02-09 18:08:32 -0500

    renames

--- src/Flutes.jl
+++ src/Flutes.jl
@@ -9,7 +9,8 @@ time in seconds
 """
 module Flutes
 
-export tubelength, holelength
+export soundspeed, halfwavelength
+export flutelength, toneholelength
 export 𝐺
 
 """
@@ -45,13 +46,13 @@ function halfwavelength(𝑓=440.0, 𝜗=25.0)
 end
 
 """
-  ℓₜ = tubelength(;𝑓=261.615565, 𝜗=25.0, ℓᵩ=52.0, ⌀=19.0)
+  ℓₜ = flutelength(;𝑓=261.615565, 𝜗=25.0, ℓᵩ=52.0, ⌀=19.0)
 
-Calculate tube length from embouchure-hole to open-end
+Calculate flute length from embouchure-hole to open-end
   for fundamental frequency 𝑓, temperature 𝜗, embouchure correction ℓᵩ,
   and open-end bore diameter ⌀
 """
-function tubelength(𝑓=261.615565; 𝜗=25.0, ℓᵩ=52.0, ⌀=19.0)
+function flutelength(𝑓=261.615565; 𝜗=25.0, ℓᵩ=52.0, ⌀=19.0)
   𝜑 = halfwavelength(𝑓, 𝜗)
   𝛥ℓₜ = 0.3⌀
   ℓₜ = 𝜑 - ℓᵩ - 𝛥ℓₜ
@@ -59,14 +60,14 @@ function tubelength(𝑓=261.615565; 𝜗=25.0, ℓᵩ=52.0, ⌀=19.0)
 end
 
 """
-  ℓₕ = holelength(;𝑓=440, 𝜗=25.0, ℓᵩ=52.0, ⌀=19.0, ℎ=2.5, 𝑑=7, 𝑔=(𝐺 - 1))
+  ℓₕ = toneholelength(;𝑓=440, 𝜗=25.0, ℓᵩ=52.0, ⌀=19.0, ℎ=2.5, 𝑑=7, 𝑔=(𝐺 - 1))
 
 Calculate distance from embouchure hole center to tone hole center
   for supplied frequency 𝑓, temperature 𝜗, embouchure correction ℓᵩ,
   tone-hole bore diameter ⌀, tone-hole height ℎ, tone-hole diameter 𝑑,
   and interval coefficient 𝑔
 """
-function holelength(𝑓=440; 𝜗=25.0, ℓᵩ=52.0, ⌀=19.0, ℎ=2.5, 𝑑=7, 𝑔=(𝐺 - 1))
+function toneholelength(𝑓=440; 𝜗=25.0, ℓᵩ=52.0, ⌀=19.0, ℎ=2.5, 𝑑=7, 𝑔=(𝐺 - 1))
   𝜑 = halfwavelength(𝑓, 𝜗)
   𝐿 = (ℎ+𝑑) * (⌀/𝑑)^2 - 0.45⌀
   𝑧 = 𝑔/2 * √(1 + 4𝐿/(𝑔*𝜑)) - 𝑔/2
