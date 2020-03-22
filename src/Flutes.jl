c6fbdc27ff8cfc6110c23a308ff3dd88ca2687d7
Isaac W Hanson <isaac@starlig.ht>
2020-03-22 19:22:18 -0400

    fix tests

--- src/Flutes.jl
+++ src/Flutes.jl
@@ -1,6 +1,5 @@
 module Flutes
   include("tones.jl")
-  include("struct.jl")
   include("calc.jl")
   include("search.jl")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using Flutes
 using Test
 
-@test Flutes.soundspeed(0.0) == 331300
-@test Flutes.soundspeed(25.0) == 346100
+@test Flutes.soundspeed(0.0) == 331316.0
+@test Flutes.soundspeed(25.0) == 346146.0
+@test Flutes.note("A4") == 440.0
+@test Flutes.note("C♯0") == 17.323914
+@test Flutes.note("C♭8") == 3951.06641
