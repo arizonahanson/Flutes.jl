84873d03f6a3cb1e22c333452236abb83907d5da
Isaac W Hanson <isaac@starlig.ht>
2020-04-20 18:57:47 -0400

    comments

--- src/lib/search.jl
+++ src/lib/search.jl
@@ -26,34 +26,34 @@ end
 
 # error function factory (constraints)
 function mkerrfn(flute::FluteConstraint)
-  # return error function
+  # list of frequencies in descending pitch
   𝒇 = map(𝒉->𝒉.𝑓, flute.holes); push!(𝒇, flute.𝑓)
   𝑯 = 1:length(flute.holes)
   function errfn(𝒅)
-    𝑒 = 0.0 # error
-    ℓₚ = 0.0 # position of previous hole, or embouchure
+    𝑒 = 0.0 # error result
+    ℓₚ = 0.0 # location of previous hole, or embouchure
     𝛥𝜆ᵥ = 0.0 # closed-hole correction
     𝑑mean = mean(𝒅)
-    for h in 𝑯
-      # for each tonehole calculate error
+    for h in 𝑯  # body->foot order
+      # for each tonehole calculate error based on preferences
       𝒉 = flute.holes[h]
-      𝑓 = 𝒉.𝑓
-      𝑓ₜ = 𝒇[h+1]
-      𝑑ₕ = 𝒅[h]
-      ℓₕ = toneholelength(𝑓; 𝑓ₜ=𝑓ₜ, 𝑑=𝑑ₕ, 𝛥𝜆ᵥ=𝛥𝜆ᵥ)
+      𝑓 = 𝒉.𝑓 # open hole frequency
+      𝑓ₜ = 𝒇[h+1] # closed hole frequency
+      𝑑ₕ = 𝒅[h] # proposed hole diameter
+      ℓₕ = toneholelength(𝑓; 𝑓ₜ=𝑓ₜ, 𝑑=𝑑ₕ, 𝛥𝜆ᵥ=𝛥𝜆ᵥ) # resulting location
       # deviation from reachable
-      𝛬min = ℓₚ - ℓₕ + 𝒉.𝑝₋ # positive if position below min
-      𝛬max = ℓₕ - ℓₚ - 𝒉.𝑝₊ # positive if position above max
-      𝛬near = max(𝛬min, 0.0, 𝛬max)
-      # deviation from max hole diameter
+      𝛬crowd = ℓₚ - ℓₕ + 𝒉.𝑝₋ # positive if location below min
+      𝛬stretch = ℓₕ - ℓₚ - 𝒉.𝑝₊ # positive if location above max
+      𝛬reach = max(𝛬crowd, 0.0, 𝛬stretch)
+      # deviation from max hole diameter (prefer larger diameters)
       𝛬big = abs(toneholelength(𝑓; 𝑓ₜ=𝑓ₜ, 𝑑=𝒉.𝑑₊, 𝛥𝜆ᵥ=𝛥𝜆ᵥ) - ℓₕ)
-      # deviation from mean hole diameter
+      # deviation from mean hole diameter (prefer average diameters)
       𝛬avg = abs(toneholelength(𝑓; 𝑓ₜ=𝑓ₜ, 𝑑=𝑑mean, 𝛥𝜆ᵥ=𝛥𝜆ᵥ) - ℓₕ)
-      # sum weighted errors
-      𝑒 += 2𝛬near^2 + 𝛬big + 𝛬avg/2
+      # sum weighted errors (heavy weight on reachable locations)
+      𝑒 += 2𝛬reach^2 + 𝛬big + 𝛬avg/2
       # calculate increased correction for next loop
       𝛥𝜆ᵥ += closedholecorrection(𝒉.𝑓; 𝑓ₜ=𝑓ₜ, 𝑑=𝑑ₕ, 𝛥𝜆ᵥ=𝛥𝜆ᵥ)
-      # next loop use this hole as previous hole
+      # next loop use this hole as the previous hole
       ℓₚ = ℓₕ
     end
     return 𝑒
