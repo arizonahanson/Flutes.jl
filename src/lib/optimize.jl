a7e1df25dc2a15c06d80687885e7dd7c3ff15700
Isaac W Hanson <isaac@starlig.ht>
2020-07-04 20:37:38 -0400

    plop=>lop; adjust weights

--- src/lib/optimize.jl
+++ src/lib/optimize.jl
@@ -13,21 +13,21 @@ function Σ∇(𝒍₋, 𝒍₊, 𝒍)
   return sum(map((ℓ₋, ℓ₊, ℓ)->max(ℓ₋-ℓ, 0.0, ℓ-ℓ₊), 𝒍₋, 𝒍₊, 𝒍))
 end
 
-# all but last element
-function plop(𝒍)
+# return all but last element
+function lop(𝒍)
   return 𝒍[1:end-1]
 end
 
 # error function factory (constraints)
 function mkerrfn(flute::FluteConstraint)
-  𝒍max = plop(mapflute(flute, map(𝒉->𝒉.𝑑₊, flute.holes))) # positions of max diameters
+  𝒍max = lop(mapflute(flute, map(𝒉->𝒉.𝑑₊, flute.holes))) # positions of max diameters
   function errfn(𝒅)
-    𝒍 = plop(mapflute(flute, 𝒅)) # hole positions
-    𝒍mean = plop(mapflute(flute, fill(mean(𝒅), length(flute.holes))) # positions of mean diameters
-    𝒍prev = prepend!(plop(𝒍), 0.0)
+    𝒍 = lop(mapflute(flute, 𝒅)) # hole positions
+    𝒍mean = lop(mapflute(flute, fill(mean(𝒅), length(flute.holes)))) # positions of mean diameters
+    𝒍prev = prepend!(lop(𝒍), 0.0)
     𝒍close = map((ℓₚ, 𝒉)->ℓₚ+𝒉.𝑝₋, 𝒍prev, flute.holes)
     𝒍far = map((ℓₚ, 𝒉)->ℓₚ+𝒉.𝑝₊, 𝒍prev, flute.holes)
-    𝑒 = ΣΔ(𝒍max, 𝒍) + ΣΔ(𝒍mean, 𝒍) + 2*Σ∇(𝒍close, 𝒍far, 𝒍)^2
+    𝑒 = ΣΔ(𝒍max, 𝒍) + 2ΣΔ(𝒍mean, 𝒍) + 2*Σ∇(𝒍close, 𝒍far, 𝒍)^2
     return 𝑒
   end
   return errfn
@@ -36,7 +36,7 @@ end
 function minbox(flute::FluteConstraint)
   𝒅₋ = map(𝒉->𝒉.𝑑₋, flute.holes)
   𝒅₊ = map(𝒉->𝒉.𝑑₊, flute.holes)
-  𝒅₀ = map(𝒅->0.9𝒅, (𝒅₊-𝒅₋)) + 𝒅₋
+  𝒅₀ = map((𝑑₊, 𝑑₋)->0.9(𝑑₊-𝑑₋)+𝑑₋, 𝒅₊, 𝒅₋)
   return (𝒅₋, 𝒅₊, 𝒅₀)
 end
 
