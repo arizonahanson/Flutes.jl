de19660754ebd132f6624a63c0872fcfa5149a88
Isaac W Hanson <isaac@starlig.ht>
2020-07-14 17:38:16 -0400

    rename/refactor

--- src/lib/optimize.jl
+++ src/lib/optimize.jl
@@ -4,33 +4,29 @@ using Statistics
 
 
 # sum of elementwise differences
-function Δ⃯(𝒍₁, 𝒍₂)
-  return sum(map(abs, 𝒍₂- 𝒍₁))
+function Δ⃯(𝒄₁, 𝒄₂)
+  return sum(map(abs, 𝒄₁- 𝒄₂))
 end
 
 # sum of distance outside box
-function Δ͍(𝒍, 𝒍₋, 𝒍₊)
-  return (sum(map((ℓ₋, ℓ₊, ℓ)->max(ℓ₋-ℓ, 0.0, ℓ-ℓ₊), 𝒍₋, 𝒍₊, 𝒍)) + 1)^2 - 1
+function Δ͍(𝒄, 𝒄₋, 𝒄₊)
+  return sum(max.(𝒄₋-𝒄, 0.0, 𝒄-𝒄₊))
 end
 
 # return all but last element
-function drop(𝒍)
-  return 𝒍[1:end-1]
+function front(𝒄)
+  return 𝒄[1:end-1]
 end
 
-# return 𝒍 with ℓ as first element inserted
-function push(ℓ, 𝒍)
-  return [ℓ; 𝒍]
-end
-
-function avg(𝒅)
-  return fill(mean(𝒅), length(𝒅))
+# same-length collection of average value
+function avg(𝒄)
+  return fill(mean(𝒄), length(𝒄))
 end
 
 # error function factory (constraints)
 function mkerrfn(flute::FluteConstraint)
-  ⇴ = drop ∘ mapflute
-  ⬰ = drop ∘ push
+  ⇴ = front ∘ mapflute
+  ⬰ = front ∘ vcat
   𝒉 = flute.holes
   𝒇 = [map(ℎ->ℎ.𝑓, 𝒉); flute.𝑓]
   𝒑₋ = map(ℎ->ℎ.𝑝₋, 𝒉)
@@ -39,17 +35,17 @@ function mkerrfn(flute::FluteConstraint)
   𝒍⃯ = 𝒇⇴ 𝒅₊
   function errfn(𝒅)
     # locations
-    𝒍  = 𝒇⇴ 𝒅
-    𝒍ᵪ̅ = 𝒇⇴ avg(𝒅)
-    𝒍⃮  = 0.0⬰ 𝒍
-    𝒍͍₋ = 𝒍⃮+𝒑₋
-    𝒍͍₊ = 𝒍⃮+𝒑₊
+    𝒍 = 𝒇⇴ 𝒅
+    𝒍̲ = 𝒇⇴ avg(𝒅)
+    𝒍⃮ = 0.0⬰ 𝒍
+    𝒍⃭ = 𝒍⃮+𝒑₋
+    𝒍⃬ = 𝒍⃮+𝒑₊
     # error terms
-    𝑒ᵪ̅ = Δ⃯(𝒍, 𝒍ᵪ̅)
-    𝑒⃯  = Δ⃯(𝒍, 𝒍⃯)
-    𝑒͍  = Δ͍(𝒍, 𝒍͍₋, 𝒍͍₊)
+    𝑒̲ = Δ⃯(𝒍, 𝒍̲)
+    𝑒⃯ = Δ⃯(𝒍, 𝒍⃯)
+    𝑒͍ = Δ͍(𝒍, 𝒍⃭, 𝒍⃬)^2
     # sum and weigh
-    𝑒  = 𝑒ᵪ̅ + 2𝑒⃯ + 3𝑒͍
+    𝑒 = 𝑒̲ + 2𝑒⃯ + 3𝑒͍
     return 𝑒
   end
   return errfn
